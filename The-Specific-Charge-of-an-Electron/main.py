62e2303a948298486205592b2c35fe604da35459
MihaiZegheru <mihai.z.2005@gmail.com>
2024-10-16 20:09:35 +0300

    Updated The-Specific-Charge-of-an-Electron.
    Signed-off-by: MihaiZegheru <mihai.z.2005@gmail.com>

--- The-Specific-Charge-of-an-Electron/main.py
+++ The-Specific-Charge-of-an-Electron/main.py
@@ -2,6 +2,8 @@ from math import sqrt, pi
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
+import numpy as np
+from decimal import Decimal
 
 IN_PATH = "The-Specific-Charge-of-an-Electron/in/"
 OUT_PATH = "The-Specific-Charge-of-an-Electron/out/"
@@ -10,6 +12,18 @@ N = 5
 u0 = 4 * pi * 1E-7
 n = 154
 R = 0.2
+
+# From https://stackoverflow.com/questions/6913532/display-a-decimal-in-scientific-notation
+def format_decimal(x, prec=3):
+    tup = x.as_tuple()
+    digits = list(tup.digits[:prec + 1])
+    sign = '-' if tup.sign else ''
+    dec = ''.join(str(i) for i in digits[1:])
+    exp = x.adjusted()
+    return '{sign}{int}.{dec}E{exp}'.format(sign=sign, int=digits[0], dec=dec, exp=exp)
+
+def toSci(num):
+	return format_decimal(Decimal(num))
 
 def computeAverageCurrentValue(currents):
 	return sum(currents) / len(currents)
@@ -32,9 +46,12 @@ def computeExperimentalCharges(currents, r, U):
 		charges.append(computeCharge(r, U, computeMagneticField(curr)))
 	return charges
 
-def plotLinearRegression(x, y, xlabel, ylabel):
+def plotLinearRegression(x, y, xlabel, ylabel, title):
 	slope, intercept, r, p, std_err = stats.linregress(x, y)
 	foo = lambda x : x * slope + intercept
+	plt.figure(figsize=(8,7))
+	plt.title(title)
+	plt.get_current_fig_manager().set_window_title(title)
 	plt.scatter(x, y)
 	plt.plot(x, list(map(foo, x)))
 	plt.xlabel(xlabel)
@@ -42,7 +59,6 @@ def plotLinearRegression(x, y, xlabel, ylabel):
 	plt.figtext(0.5, 0.01, f"y = x * {slope} + {intercept}", ha="center",
 						 fontsize=13)
 	a = (125 / 32) * (R**2 / (u0**2 * n**2)) * (1/slope)
-	print(a)
 	plt.show()
 
 def taskOne():
@@ -58,73 +74,70 @@ def taskOne():
 																		 df.loc[idx, 'B'])
 		charges = computeExperimentalCharges(currents, df.loc[idx, 'r'],
 																			 df.loc[idx, 'U'])
-		plotLinearRegression(currents, charges, 'I(A)', 'e/m (C/kg)')
+		plotLinearRegression(currents, charges, 'I(A)', 'e/m (C/kg)', "r=25mm - const, U=160V - const")
+	for idx, row in df.iterrows():
+		df.loc[idx, 'Im'] = toSci(df.loc[idx, 'Im'])
+		df.loc[idx, 'SIm'] = toSci(df.loc[idx, 'SIm'])
+		df.loc[idx, 'EIm'] = toSci(df.loc[idx, 'EIm'])
+		df.loc[idx, 'B'] = toSci(df.loc[idx, 'B'])
+		df.loc[idx, 'e/m'] = toSci(df.loc[idx, 'e/m'])
 	df.to_csv(OUT_PATH + "data1.csv");
 					  
-def solveForR(df):
-	print(df)
+def solveForR(gf, df):
 	x = []
 	y = []
 	idx = 0
-	for idx, row in df.iterrows():
+	for idx, row in gf.iterrows():
 		df.loc[idx, 'e/m'] = computeCharge(df.loc[idx, 'r'], df.loc[idx, 'U'],
 																		 computeMagneticField(df.loc[idx, 'I']))
-		df.loc[idx, 'Im'] = df['I'].mean()
+		df.loc[idx, 'Im'] = gf['I'].mean()
 		x.append(df.loc[idx, 'U'] / (df.loc[idx, 'r']**2))
 		y.append(df.loc[idx, 'I']**2)
-
-	for idx, row in df.iterrows():
-		df.loc[idx, 'SIm'] = computeStandardDeviationOfCurrent(df['I'], df.loc[idx, 'Im'])
+	for idx, row in gf.iterrows():
+		df.loc[idx, 'SIm'] = computeStandardDeviationOfCurrent(gf['I'], df.loc[idx, 'Im'])
 		df.loc[idx, 'EIm'] = df.loc[idx, 'SIm'] / df.loc[idx, 'Im']
-	df.to_csv(OUT_PATH + f"data2-r={df.loc[idx, 'r']}.csv")
-	plotLinearRegression(x, y, 'U/r^2 (V/m)', 'I^2 (A^2)')
+	
+	plotLinearRegression(x, y, 'U/r^2 (V/m)', 'I^2 (A^2)', f"r={df.loc[idx, 'r']} - const")
+	for idx, row in gf.iterrows():
+		df.loc[idx, 'Im'] = toSci(df.loc[idx, 'Im'])
+		df.loc[idx, 'SIm'] = toSci(df.loc[idx, 'SIm'])
+		df.loc[idx, 'EIm'] = toSci(df.loc[idx, 'EIm'])
+		df.loc[idx, 'e/m'] = toSci(df.loc[idx, 'e/m'])
 
 def taskTwo():
 	df = pd.read_csv(IN_PATH + "data2.csv")
-	df.groupby(['r']).apply(solveForR)
+	df.groupby(['r']).apply(solveForR, df)
+	df.to_csv(OUT_PATH + "data2.csv")
 
-def plotForU(df):
+def solveForU(gf, df):
 	x = []
 	y = []
-	for idx, row in df.iterrows():
-		x.append(df.loc[idx, 'U'] / (df.loc[idx, 'r']**2))
-		y.append(df.loc[idx, 'I']**2)
-	plotLinearRegression(x, y, 'U/r^2 (V/m)', 'I^2 (A^2)')
-
-def solveForU(df, Ims, ps):
-	for idx, row in df.iterrows():
+	idx = 0
+	for idx, row in gf.iterrows():
 		df.loc[idx, 'e/m'] = computeCharge(df.loc[idx, 'r'], df.loc[idx, 'U'],
 																		 computeMagneticField(df.loc[idx, 'I']))
-		# df.loc[idx, 'Im'] = df.groupby(['U'] == df.loc[idx, 'U'])['I'].mean()
-		print(df.loc[idx, 'e/m'] * 1E-11)
-		df.loc[idx, '1/r'] = 1 / df.loc[idx, 'r']
-	df.to_csv(OUT_PATH + "data3.csv")
-
-def solveCompositesForU(df):
-	Ims = []
-	ps = []
-	for gr in df.groupby(['U']):
-		Ims.append(gr[1]['I'].mean())
-
-		x = []
-		y = []
-		for idx, row in gr[1].iterrows():
-			x.append(df.loc[idx, 'U'] / (df.loc[idx, 'r']**2))
-			y.append(df.loc[idx, 'I']**2)
-		slope, intercept, r, p, std_err = stats.linregress(x, y)
-		ps.append(slope)
-	return (Ims, ps)
-
+		df.loc[idx, 'Im'] = gf['I'].mean()
+		x.append(1 / df.loc[idx, 'r'])
+		y.append(df.loc[idx, 'I'])
+	for idx, row in gf.iterrows():
+		df.loc[idx, 'SIm'] = computeStandardDeviationOfCurrent(gf['I'], df.loc[idx, 'Im'])
+		df.loc[idx, 'EIm'] = df.loc[idx, 'SIm'] / df.loc[idx, 'Im']
+	
+	plotLinearRegression(x, y, '1/r (m^-1)', 'I (A)', f"U={df.loc[idx, 'U']} - const")
+	for idx, row in gf.iterrows():
+		df.loc[idx, 'Im'] = toSci(df.loc[idx, 'Im'])
+		df.loc[idx, 'SIm'] = toSci(df.loc[idx, 'SIm'])
+		df.loc[idx, 'EIm'] = toSci(df.loc[idx, 'EIm'])
+		df.loc[idx, 'e/m'] = toSci(df.loc[idx, 'e/m'])
 	
 def taskThree():
 	df = pd.read_csv(IN_PATH + "data3.csv")
-	# (Ims, ps) = solveCompositesForU(df);
-	# solveForU(df, Ims, ps)
-	df.groupby(['U']).apply(solveForU)
+	df.groupby(['U']).apply(solveForU, df)
+	df.to_csv(OUT_PATH + "data3.csv")
 
-# taskOne()
+taskOne()
 taskTwo()
-# taskThree()
+taskThree()
 # print(df)
 
 
